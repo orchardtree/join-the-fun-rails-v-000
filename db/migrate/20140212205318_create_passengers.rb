ef6b1dfcb66d93a836ea419917e37eac12d3f28a
Jesse Wenger <wengerj@gmail.com>
2021-05-01 19:31:03 +0000

    Automatically backed up by Learn

--- db/migrate/20140212205318_create_passengers.rb
+++ db/migrate/20140212205318_create_passengers.rb
@@ -1,7 +1,7 @@
 class CreatePassengers < ActiveRecord::Migration
   def change
     create_table :passengers do |t|
-      t.number ride_id
+      t.integer :ride_id
       t.timestamps null: false
     end
   end
