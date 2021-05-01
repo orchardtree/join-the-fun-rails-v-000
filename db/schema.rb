c2bd1403b0189f5266b51e5d294e82a8e99c51a6
Jesse Wenger <wengerj@gmail.com>
2021-05-01 19:31:30 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -13,16 +13,20 @@
 ActiveRecord::Schema.define(version: 20140212205318) do
 
   create_table "passengers", force: :cascade do |t|
+    t.integer  "ride_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "rides", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "taxi_id"
+    t.integer  "passenger_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "taxis", force: :cascade do |t|
+    t.integer  "ride_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
